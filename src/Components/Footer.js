98a3cfbb7c426d9ae79465e397164be8dfe2fd54
Mr-Yash-01 <mr.yashvadukiya.2003@gmail.com>
2024-06-11 23:21:19 +0530

    final

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -3,7 +3,7 @@ import React from 'react'
 const Footer = () => {
   return (
       <div  className='text-gray-800 mt-6 font-bold flex items-center justify-end'>
-        <a className='flex gap-2' href='https://github.com/Mr-Yash-01/ProdigyInfoTech/tree/master/PRODIGY_WD_05/weather'>
+        <a className='flex gap-2' href='https://github.com/Mr-Yash-01/weather'>
         <img alt='logo' className='w-6 h-6' src='https://img.icons8.com/?size=100&id=4i8EFdsgyQ2M&format=png&color=000000'></img>
         <span>Yash Vadukiya</span>
         </a>
